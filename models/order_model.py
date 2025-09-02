141aa1b8e4d5d3ca9e34a6d88dc5078bcde6572d
LuKe <demon80706@gmail.com>
2025-09-02 21:48:34 +0630

    Update order_model.py

--- models/order_model.py
+++ models/order_model.py
@@ -18,3 +18,9 @@ def create_order(user_id, game, package, account_id, server_id=None):
 
 def get_orders(user_id):
     return list(orders_col.find({"user_id": user_id}, {"_id": 0}))
+
+def get_order_by_id(order_id):
+    return orders_col.find_one({"_id": order_id})
+
+def update_order_status(order_id, status):
+    orders_col.update_one({"_id": order_id}, {"$set": {"status": status}})
