0d8ba5c90c1632b30c32916ffd14b60b7ff3f858
LuKe <demon80706@gmail.com>
2025-09-02 21:37:30 +0630

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from routes.auth_routes import auth_bp
+from routes.user_routes import user_bp
+from routes.task_routes import task_bp
+from routes.refer_routes import refer_bp
+from routes.order_routes import order_bp
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+
+# Register Blueprints
+app.register_blueprint(auth_bp, url_prefix="/api/auth")
+app.register_blueprint(user_bp, url_prefix="/api/user")
+app.register_blueprint(task_bp, url_prefix="/api/tasks")
+app.register_blueprint(refer_bp, url_prefix="/api/refer")
+app.register_blueprint(order_bp, url_prefix="/api/order")
+
+if __name__ == "__main__":
+    app.run(debug=(os.getenv("FLASK_ENV")=="development"))
