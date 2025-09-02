8b71d5fc0fa32ba39948778ea801625f956554f5
LuKe <demon80706@gmail.com>
2025-09-02 21:40:02 +0630

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/gamevault")
+MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["gamevault"]
 
-JWT_SECRET = os.getenv("JWT_SECRET", "supersecretkey")
+JWT_SECRET = os.getenv("JWT_SECRET")
 FLASK_ENV = os.getenv("FLASK_ENV", "production")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 ADSGRAM_BLOCK_ID = os.getenv("ADSGRAM_BLOCK_ID")
